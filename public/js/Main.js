0f89c72dfa235e08d52e9a756126a6a8ced8e5ad
Taylor Lowery <admiral.taylor@gmail.com>
2017-06-05 20:12:15 -0700

    hopefully adding hello world

---
+++ public/js/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../css/Main.css'
+
+class Main extends React.Component{
+  render(){
+    return(
+      <h1>Eyyy Lmao</h1>
+    )
+  }
+}
+
+export default Main;
