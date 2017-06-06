83c77652f7fb868aaad9a88e7b370d0f21e18eaa
Brad <bradleynielsen@gmail.com>
2017-06-05 21:40:55 -0500

    added app

--- app/index.js
+++ app/index.js
@@ -1,2 +1,11 @@
 import React from 'react'
 import {Router, hashHistory,Route} from 'react-router'
+
+
+export default () => (
+    <Router history={hashHistory}>
+      <Route component={Root}>
+        <Route path="/" component={Main}  />
+      </Route>
+    </Router>
+  )
