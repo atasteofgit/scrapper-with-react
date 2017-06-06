74a7f6dcf1e6314c744acb3201c64bf43243c528
Taylor Lowery <admiral.taylor@gmail.com>
2017-06-05 20:00:34 -0700

    fuckin with the webpack

---
+++ public/js/SearchCard.js
@@ -0,0 +1,11 @@
+ <div>
+        <h1 class="indeedLogo">indeed</h1>
+      </div>
+      <div class="searchWrapper">
+        <form method ="post" action="/listings">
+          <input name ="job" type="text" id="searchTitle" placeholder="Job Title"></input>
+          <input name ="location" type="text" id="searchLocation" placeholder="Location"></input>
+          <input id="seachButton" type="Submit" value="Search" onclick="empty()"></input>
+        </form>
+        <p id="gitLink1" >The code for this project can be viewed on <a href="https://github.com/admiraltaylor/indeed-scraper">GitHub</a></p>
+      </div>
