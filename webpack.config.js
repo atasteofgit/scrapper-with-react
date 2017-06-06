3d8b09ed00a6976779106df30bcdc12dd98cff1e
Taylor Lowery <admiral.taylor@gmail.com>
2017-06-05 21:04:00 -0700

    yo

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 
 module.exports = {
 
-    entry: path.resolve(__dirname, 'app') + '/index.js',
+    entry: path.resolve(__dirname, 'app') + '/app.js',
     output: {
         path: path.resolve(__dirname, 'public'),
         filename: 'bundle.js',
